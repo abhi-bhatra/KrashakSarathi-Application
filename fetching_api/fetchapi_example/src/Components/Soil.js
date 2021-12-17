9d8aa618de422c7bfd1edc4f77ccb3a75ec2632f
= <=>
2021-12-17 21:42:42 +0530

    Add Kisan API

---
+++ fetching_api/fetchapi_example/src/Components/Soil.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import axios from "axios";
+
+function Soil() {
+    const [post, setPost] = React.useState([]);
+    
+    React.useEffect(() => {
+        axios.get("https://api.ambeedata.com/soil/latest/by-lat-lng", {headers: {'x-api-key': 'faf80debeadf9f5107682d7b4de3a70a14f971801bbea36f9782008c0b5b5675','Content-type': 'application/json'}, params: {lat: '51.5', lng: '-0.1'}}).then((response) => {
+          setPost(response.data);
+          console.log(response.data);
+        });
+      }, []);
+
+    if (!post) return null;
+
+    return (
+        <div>
+      <h1>Soil Moisture and Temperature</h1>
+      <h1>{post.soil_temperature}</h1>
+    </div>
+    );
+}
+
+export default Soil;
---
+++ fetching_api/fetchapi_example/src/Components/Weather.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import axios from "axios";
+
+function Soil() {
+    const [post, setPost] = React.useState([]);
+    
+    React.useEffect(() => {
+        axios.get("https://api.ambeedata.com/weather/forecast/by-lat-lng", {headers: {'x-api-key': 'faf80debeadf9f5107682d7b4de3a70a14f971801bbea36f9782008c0b5b5675','Content-type': 'application/json'}, params: {lat: '51.5', lng: '-0.1', filter: 'daily'}}).then((response) => {
+          setPost(response.data);
+          console.log(response.data);
+        });
+      }, []);
+
+    if (!post) return null;
+
+    return (
+        <div>
+            <h1>Weather Forecast</h1>
+        </div>
+    );
+}
+
+export default Soil;
+
+
