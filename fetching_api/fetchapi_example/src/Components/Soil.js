fc78d8fb67a4934c4810f7c91eb1063eb69c1fe3
= <=>
2021-12-27 21:57:25 +0530

    fixing API

--- fetching_api/fetchapi_example/src/Components/Soil.js
+++ fetching_api/fetchapi_example/src/Components/Soil.js
@@ -4,21 +4,30 @@ import axios from "axios";
 function Soil() {
     const [post, setPost] = React.useState([]);
     
+    const fetchData = React.useCallback(() => {
+      axios.get("https://api.ambeedata.com/soil/latest/by-lat-lng", {headers: {'x-api-key': 'faf80debeadf9f5107682d7b4de3a70a14f971801bbea36f9782008c0b5b5675','Content-type': 'application/json'}, params: {lat: '51.5', lng: '-0.1'}}).then((response) => {
+        const APIResponse = response.data;
+        console.log(APIResponse.data);
+        setPost(APIResponse.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+    }, [])
+
     React.useEffect(() => {
-        axios.get("https://api.ambeedata.com/soil/latest/by-lat-lng", {headers: {'x-api-key': 'faf80debeadf9f5107682d7b4de3a70a14f971801bbea36f9782008c0b5b5675','Content-type': 'application/json'}, params: {lat: '51.5', lng: '-0.1'}}).then((response) => {
-          setPost(response.data);
-          console.log(response.data);
-        });
-      }, []);
+      fetchData();
+      }, [fetchData]);
 
     if (!post) return null;
 
     return (
         <div>
       <h1>Soil Moisture and Temperature</h1>
-      <h1>{post.soil_temperature}</h1>
-      {post.map((edata) => (
-        <p>{edata.soil_moisture}</p>
+      {post.map((item, idx) => (
+        <p key={idx}>{item.soil_moisture} and 
+        <br />
+        {item.soil_temperature}</p>
       ))}
     </div>
     );
--- fetching_api/fetchapi_example/src/Components/Weather.js
+++ fetching_api/fetchapi_example/src/Components/Weather.js
@@ -4,20 +4,29 @@ import axios from "axios";
 function Soil() {
     const [post, setPost] = React.useState([]);
     
+    const fetchData = React.useCallback(() => {
+      axios.get("https://api.ambeedata.com/weather/forecast/by-lat-lng", {headers: {'x-api-key': 'faf80debeadf9f5107682d7b4de3a70a14f971801bbea36f9782008c0b5b5675','Content-type': 'application/json'}, params: {lat: '51.5', lng: '-0.1', filter: 'daily'}}).then((response) => {
+        const APIResponse = response.data;
+        console.log(APIResponse.data.forecast);
+        setPost(APIResponse.data.forecast);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+    }, [])
+
     React.useEffect(() => {
-        axios.get("https://api.ambeedata.com/weather/forecast/by-lat-lng", {headers: {'x-api-key': 'faf80debeadf9f5107682d7b4de3a70a14f971801bbea36f9782008c0b5b5675','Content-type': 'application/json'}, params: {lat: '51.5', lng: '-0.1', filter: 'daily'}}).then((response) => {
-          setPost(response.data.forecast);
-          console.log(response.data.forecast);
-        });
-      }, []);
+        fetchData();
+      }, [fetchData]);
 
     if (!post) return null;
 
     return (
         <div>
             <h1>Weather Forecast</h1>
-            {post.map((edata) => (
-              <p>{edata[0]}</p>
+            {post.map((item, idx) => (
+              <p key={idx}>{item.summary}
+              <br /></p>
             ))}
         </div>
     );
