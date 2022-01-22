243d639e20900ca4ab6d7c03e5b632e0838c5523
= <=>
2022-01-22 17:50:39 +0530

    Add microservice architecture

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -9,7 +9,7 @@ export const Weather = (props) => {
           var lat = position.coords.latitude;
           var long = position.coords.longitude; 
           
-          axios.get(`https://api.ambeedata.com/soil/latest/by-lat-lng?lat=${lat}&lng=${long}`, {headers: {'x-api-key': '4e01913f5204a97c2e44ff85c406b6bb8ebde8b8a63d4037210cc700c3b2350d','Content-type': 'application/json'}}).then((response) => {
+          axios.get(`https://api.ambeedata.com/weather/forecast/by-lat-lng?lat=${lat}&lng=${long}&filter=daily`, {headers: {'x-api-key': '4e01913f5204a97c2e44ff85c406b6bb8ebde8b8a63d4037210cc700c3b2350d','Content-type': 'application/json'}}).then((response) => {
           const APIResponse = response.data;
           console.log(APIResponse.data.forecast);
           setPost(APIResponse.data.forecast);
@@ -27,17 +27,17 @@ export const Weather = (props) => {
         if (!post) return null;
 
     return (
-        <div id='soil' className="text-center">
+        <div id='weather' className="text-center">
             <div className="container">
                 <div className="col-md-10 col-md-offset-1 section-title">
                     <h2>Weather Forecasting</h2>
                 </div>
                 <div className="row">
+                <div className="col-xs-6 col-md-3">
                     {post.map((item, idx) => (
-                        <div key={idx} className="col-xs-6 col-md-3">
-                            <h3>Day {idx+1}: {item.summary} <b>%</b></h3>
-                        </div>
+                        <h3 key={idx}>Day {idx+1}: {item.summary} </h3>
                     ))}
+                </div>
                 </div>
             </div>
         </div>
